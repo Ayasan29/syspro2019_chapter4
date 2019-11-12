856490464cede3d65badb4019db36339e91e4c91
Ayasan29 <s16041@tokyo.kosen-ac.jp>
2019-11-12 10:02:57 +0900

    setservo関数追加

--- setservo.py
+++ setservo.py
@@ -1,5 +1,4 @@
 import time
-import sys
 import RPi.GPIO as GPIO
 
 GPIO.setmode(GPIO.BCM)
@@ -7,13 +6,15 @@ GPIO.setup(2, GPIO.OUT)
 servo = GPIO.PWM(2, 50)
 servo.start(0.0)
 
-print("Šp“x(-90‹`90)‚ð“ü—Í")
-angle = input()
 
-pwm = (0.5 + (1.9 / 180) * angle) / 20 * 100
+def setservo(angle);
+	pwm = (0.5 + 1.9 * (angle / 180)) / 20 * 100
 	servo.ChangeDutyCycle(pwm)
 	time.sleep(1.0)
 
 
+print("enter the angle(-90 ~ 90) => ")
+angle = input()
+setservo(angle)
 
-
+GPIO.cleanup()
