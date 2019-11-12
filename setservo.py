b589149655190190f05a5db124550a20223ba447
Ayasan29 <s16041@tokyo.kosen-ac.jp>
2019-11-12 10:06:09 +0900

    修正

--- setservo.py
+++ setservo.py
@@ -7,7 +7,7 @@ servo = GPIO.PWM(2, 50)
 servo.start(0.0)
 
 
-def setservo(angle);
+def setservo(angle):
 	pwm = (0.5 + 1.9 * (angle / 180)) / 20 * 100
 	servo.ChangeDutyCycle(pwm)
 	time.sleep(1.0)
